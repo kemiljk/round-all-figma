d147d18e90971b12642c15ce203725fd460ee20b
Karl Koch <hi@kejk.co>
2021-02-06 14:30:47 +0000

    fix: children of frames are now handled

--- code.js
+++ code.js
@@ -120,6 +120,151 @@ figma.ui.onmessage = (msg) => {
                             n.y = Math.round(Y);
                             n.resize(Math.round(W), Math.round(H));
                         }
+                        if (n.type === "FRAME") {
+                            for (n of n.children) {
+                                if (n.type === "FRAME") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "TEXT") {
+                                    yield figma.loadFontAsync(n.fontName);
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let text = n.fontSize.toString();
+                                    let T = parseFloat(text);
+                                    n.fontSize = Math.round(T);
+                                }
+                                if (n.type === "RECTANGLE") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "COMPONENT") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "COMPONENT_SET") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "INSTANCE") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "VECTOR") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "STAR") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "ELLIPSE") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                                if (n.type === "POLYGON") {
+                                    let X = n.x;
+                                    let Y = n.y;
+                                    let W = n.width;
+                                    let H = n.height;
+                                    let S = n.strokeWeight;
+                                    n.strokeWeight = Math.round(S);
+                                    n.x = Math.round(X);
+                                    n.y = Math.round(Y);
+                                    n.resize(Math.round(W), Math.round(H));
+                                    let radius = n.cornerRadius.toString();
+                                    let CR = parseInt(radius);
+                                    n.cornerRadius = Math.round(CR);
+                                }
+                            }
+                        }
                         if (n.type === "GROUP") {
                             let X = n.x;
                             let Y = n.y;
