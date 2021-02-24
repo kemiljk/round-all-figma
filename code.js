359bd3d1a27f62155a19ddc13615d51ae9bd7153
Karl Koch <hi@kejk.co>
2021-02-24 12:23:15 +0000

    feat: some tweaks to handling nodes

--- code.js
+++ code.js
@@ -7,10 +7,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-figma.showUI(__html__, { width: 300, height: 100 });
+figma.showUI(__html__, { width: 300, height: 105 });
 const { selection } = figma.currentPage;
-let nodetypes = ["FRAME", "COMPONENT", "INSTANCE"];
-let nodeshapetypes = ["VECTOR", "STAR", "ELLIPSE", "RECTANGLE", "POLYGON"];
+let nodetypes = ["FRAME", "COMPONENT", "INSTANCE", "GROUP"];
+let nodeshapetypes = [
+    "VECTOR",
+    "LINE",
+    "STAR",
+    "ELLIPSE",
+    "RECTANGLE",
+    "POLYGON",
+];
 let propertiesToRound = [
     "paddingLeft",
     "paddingRight",
@@ -20,11 +27,12 @@ let propertiesToRound = [
 ];
 figma.ui.onmessage = (msg) => {
     if (msg.type === "select-and-run") {
-        const layers = figma.currentPage.findAll();
-        figma.currentPage.selection = layers;
         function selectAndRound() {
             return __awaiter(this, void 0, void 0, function* () {
                 figma.root.children.flatMap((pageNode) => pageNode.findAll().forEach((node) => __awaiter(this, void 0, void 0, function* () {
+                    node.x = Math.round(node.x);
+                    node.y = Math.round(node.y);
+                    node.resize(Math.round(node.width), Math.round(node.height));
                     if (nodetypes.includes(node.type)) {
                         node.x = Math.round(node.x);
                         node.y = Math.round(node.y);
@@ -32,7 +40,7 @@ figma.ui.onmessage = (msg) => {
                         for (let property of propertiesToRound) {
                             node[property] = Math.round(node[property]);
                         }
-                        node.cornerRadius = Math.round(parseInt(node.cornerRadius.toString()));
+                        node.cornerRadius = Math.round(Number(node.cornerRadius));
                         for (let innerNode of node.children) {
                             innerNode.x = Math.round(innerNode.x);
                             innerNode.y = Math.round(innerNode.y);
@@ -41,16 +49,16 @@ figma.ui.onmessage = (msg) => {
                                 for (let property of propertiesToRound) {
                                     innerNode[property] = Math.round(innerNode[property]);
                                 }
-                                innerNode.cornerRadius = Math.round(parseInt(innerNode.cornerRadius.toString()));
+                                innerNode.cornerRadius = Math.round(Number(innerNode.cornerRadius));
                             }
                             if (nodeshapetypes.includes(innerNode.type)) {
                                 innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
-                                innerNode.cornerRadius = Math.round(parseInt(innerNode.cornerRadius.toString()));
+                                innerNode.cornerRadius = Math.round(Number(innerNode.cornerRadius));
                             }
                             if (innerNode.type === "TEXT") {
                                 yield figma.loadFontAsync(innerNode.fontName);
                                 innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
-                                innerNode.fontSize = Math.round(parseFloat(innerNode.fontSize.toString()));
+                                innerNode.fontSize = Math.round(Number(innerNode.fontSize));
                                 let LH = innerNode.getRangeLineHeight(0, selection.length);
                                 if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
                                     innerNode.setRangeLineHeight(0, innerNode.characters.length, {
@@ -72,93 +80,60 @@ figma.ui.onmessage = (msg) => {
             });
         }
         selectAndRound();
+        if (selection.length > 0) {
+            figma.notify("All cleaned up!");
+        }
     }
     if (msg.type === "run") {
-        const { selection } = figma.currentPage;
         function roundPixels() {
             return __awaiter(this, void 0, void 0, function* () {
-                for (let n of selection) {
-                    if (n.type === "TEXT") {
-                        yield figma.loadFontAsync(n.fontName);
-                        n.x = Math.round(n.x);
-                        n.y = Math.round(n.y);
-                        n.resize(Math.round(n.width), Math.round(n.height));
-                        n.strokeWeight = Math.round(n.strokeWeight);
-                        n.fontSize = Math.round(parseFloat(n.fontSize.toString()));
-                        let LH = n.getRangeLineHeight(0, selection.length);
-                        if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
-                            n.setRangeLineHeight(0, n.characters.length, {
-                                value: Math.round(LH.value),
-                                unit: "PIXELS",
-                            });
+                figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
+                    node.x = Math.round(node.x);
+                    node.y = Math.round(node.y);
+                    node.resize(Math.round(node.width), Math.round(node.height));
+                    if (nodetypes.includes(node.type)) {
+                        node.x = Math.round(node.x);
+                        node.y = Math.round(node.y);
+                        node.resize(Math.round(node.width), Math.round(node.height));
+                        for (let property of propertiesToRound) {
+                            node[property] = Math.round(node[property]);
                         }
-                        let LS = n.getRangeLetterSpacing(0, selection.length);
-                        n.setRangeLetterSpacing(0, n.characters.length, {
-                            value: Math.round(LS.value),
-                            unit: "PIXELS",
-                        });
-                        n.paragraphSpacing = Math.round(n.paragraphSpacing);
-                    }
-                    if (n.type === "FRAME" ||
-                        n.type === "COMPONENT" ||
-                        n.type === "INSTANCE") {
-                        n.x = Math.round(n.x);
-                        n.y = Math.round(n.y);
-                        n.resize(Math.round(n.width), Math.round(n.height));
-                        n.paddingLeft = Math.round(n.paddingLeft);
-                        n.paddingRight = Math.round(n.paddingRight);
-                        n.paddingTop = Math.round(n.paddingTop);
-                        n.paddingBottom = Math.round(n.paddingBottom);
-                        n.itemSpacing = Math.round(n.itemSpacing);
-                        n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
-                        for (n of n.children) {
-                            if (n.type === "FRAME" ||
-                                n.type === "COMPONENT" ||
-                                n.type === "INSTANCE") {
-                                n.x = Math.round(n.x);
-                                n.y = Math.round(n.y);
-                                n.resize(Math.round(n.width), Math.round(n.height));
-                                n.paddingLeft = Math.round(n.paddingLeft);
-                                n.paddingRight = Math.round(n.paddingRight);
-                                n.paddingTop = Math.round(n.paddingTop);
-                                n.paddingBottom = Math.round(n.paddingBottom);
-                                n.itemSpacing = Math.round(n.itemSpacing);
-                                n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
+                        node.cornerRadius = Math.round(Number(node.cornerRadius));
+                        for (let innerNode of pageNode.selection.children) {
+                            innerNode.x = Math.round(innerNode.x);
+                            innerNode.y = Math.round(innerNode.y);
+                            innerNode.resize(Math.round(innerNode.width), Math.round(innerNode.height));
+                            if (nodetypes.includes(innerNode.type)) {
+                                for (let property of propertiesToRound) {
+                                    innerNode[property] = Math.round(innerNode[property]);
+                                }
+                                innerNode.cornerRadius = Math.round(Number(innerNode.cornerRadius));
                             }
-                            if (n.type === "VECTOR" ||
-                                n.type === "STAR" ||
-                                n.type === "ELLIPSE" ||
-                                n.type === "RECTANGLE" ||
-                                n.type === "POLYGON") {
-                                n.x = Math.round(n.x);
-                                n.y = Math.round(n.y);
-                                n.resize(Math.round(n.width), Math.round(n.height));
-                                n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
+                            if (nodeshapetypes.includes(innerNode.type)) {
+                                innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
+                                innerNode.cornerRadius = Math.round(Number(innerNode.cornerRadius));
                             }
-                            if (n.type === "TEXT") {
-                                yield figma.loadFontAsync(n.fontName);
-                                n.x = Math.round(n.x);
-                                n.y = Math.round(n.y);
-                                n.resize(Math.round(n.width), Math.round(n.height));
-                                n.strokeWeight = Math.round(n.strokeWeight);
-                                n.fontSize = Math.round(parseFloat(n.fontSize.toString()));
-                                let LH = n.getRangeLineHeight(0, selection.length);
+                            if (innerNode.type === "TEXT") {
+                                yield figma.loadFontAsync(innerNode.fontName);
+                                innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
+                                innerNode.fontSize = Math.round(Number(innerNode.fontSize));
+                                let LH = innerNode.getRangeLineHeight(0, selection.length);
                                 if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
-                                    n.setRangeLineHeight(0, n.characters.length, {
+                                    innerNode.setRangeLineHeight(0, innerNode.characters.length, {
                                         value: Math.round(LH.value),
                                         unit: "PIXELS",
                                     });
                                 }
-                                let LS = n.getRangeLetterSpacing(0, selection.length);
-                                n.setRangeLetterSpacing(0, n.characters.length, {
+                                let LS = innerNode.getRangeLetterSpacing(0, selection.length);
+                                innerNode.setRangeLetterSpacing(0, innerNode.characters.length, {
                                     value: Math.round(LS.value),
                                     unit: "PIXELS",
                                 });
-                                n.paragraphSpacing = Math.round(n.paragraphSpacing);
+                                innerNode.paragraphSpacing = Math.round(innerNode.paragraphSpacing);
                             }
                         }
                     }
-                }
+                })));
                 return Promise.resolve("Done!");
             });
         }
--- code.ts
+++ code.ts
@@ -1,8 +1,15 @@
-figma.showUI(__html__, { width: 300, height: 100 });
+figma.showUI(__html__, { width: 300, height: 105 });
 
 const { selection } = figma.currentPage;
-let nodetypes = ["FRAME", "COMPONENT", "INSTANCE"];
-let nodeshapetypes = ["VECTOR", "STAR", "ELLIPSE", "RECTANGLE", "POLYGON"];
+let nodetypes = ["FRAME", "COMPONENT", "INSTANCE", "GROUP"];
+let nodeshapetypes = [
+  "VECTOR",
+  "LINE",
+  "STAR",
+  "ELLIPSE",
+  "RECTANGLE",
+  "POLYGON",
+];
 let propertiesToRound = [
   "paddingLeft",
   "paddingRight",
@@ -13,11 +20,12 @@ let propertiesToRound = [
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "select-and-run") {
-    const layers = figma.currentPage.findAll();
-    figma.currentPage.selection = layers;
     async function selectAndRound(): Promise<String> {
       figma.root.children.flatMap((pageNode) =>
         pageNode.findAll().forEach(async (node) => {
+          node.x = Math.round(node.x);
+          node.y = Math.round(node.y);
+          node.resize(Math.round(node.width), Math.round(node.height));
           if (nodetypes.includes(node.type)) {
             node.x = Math.round(node.x);
             node.y = Math.round(node.y);
@@ -25,9 +33,7 @@ figma.ui.onmessage = (msg) => {
             for (let property of propertiesToRound) {
               node[property] = Math.round(node[property]);
             }
-            node.cornerRadius = Math.round(
-              parseInt(node.cornerRadius.toString())
-            );
+            node.cornerRadius = Math.round(Number(node.cornerRadius));
             for (let innerNode of node.children) {
               innerNode.x = Math.round(innerNode.x);
               innerNode.y = Math.round(innerNode.y);
@@ -40,21 +46,19 @@ figma.ui.onmessage = (msg) => {
                   innerNode[property] = Math.round(innerNode[property]);
                 }
                 innerNode.cornerRadius = Math.round(
-                  parseInt(innerNode.cornerRadius.toString())
+                  Number(innerNode.cornerRadius)
                 );
               }
               if (nodeshapetypes.includes(innerNode.type)) {
                 innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
                 innerNode.cornerRadius = Math.round(
-                  parseInt(innerNode.cornerRadius.toString())
+                  Number(innerNode.cornerRadius)
                 );
               }
               if (innerNode.type === "TEXT") {
                 await figma.loadFontAsync(innerNode.fontName as FontName);
                 innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
-                innerNode.fontSize = Math.round(
-                  parseFloat(innerNode.fontSize.toString())
-                );
+                innerNode.fontSize = Math.round(Number(innerNode.fontSize));
                 let LH = innerNode.getRangeLineHeight(0, selection.length);
                 if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
                   innerNode.setRangeLineHeight(0, innerNode.characters.length, {
@@ -82,99 +86,75 @@ figma.ui.onmessage = (msg) => {
       return Promise.resolve("Done!");
     }
     selectAndRound();
+    if (selection.length > 0) {
+      figma.notify("All cleaned up!");
+    }
   }
 
   if (msg.type === "run") {
-    const { selection } = figma.currentPage;
     async function roundPixels(): Promise<String> {
-      for (let n of selection) {
-        if (n.type === "TEXT") {
-          await figma.loadFontAsync(n.fontName as FontName);
-          n.x = Math.round(n.x);
-          n.y = Math.round(n.y);
-          n.resize(Math.round(n.width), Math.round(n.height));
-          n.strokeWeight = Math.round(n.strokeWeight);
-          n.fontSize = Math.round(parseFloat(n.fontSize.toString()));
-          let LH = n.getRangeLineHeight(0, selection.length);
-          if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
-            n.setRangeLineHeight(0, n.characters.length, {
-              value: Math.round(LH.value),
-              unit: "PIXELS",
-            });
-          }
-          let LS = n.getRangeLetterSpacing(0, selection.length);
-          n.setRangeLetterSpacing(0, n.characters.length, {
-            value: Math.round(LS.value),
-            unit: "PIXELS",
-          });
-          n.paragraphSpacing = Math.round(n.paragraphSpacing);
-        }
-        if (
-          n.type === "FRAME" ||
-          n.type === "COMPONENT" ||
-          n.type === "INSTANCE"
-        ) {
-          n.x = Math.round(n.x);
-          n.y = Math.round(n.y);
-          n.resize(Math.round(n.width), Math.round(n.height));
-          n.paddingLeft = Math.round(n.paddingLeft);
-          n.paddingRight = Math.round(n.paddingRight);
-          n.paddingTop = Math.round(n.paddingTop);
-          n.paddingBottom = Math.round(n.paddingBottom);
-          n.itemSpacing = Math.round(n.itemSpacing);
-          n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
-          for (n of n.children) {
-            if (
-              n.type === "FRAME" ||
-              n.type === "COMPONENT" ||
-              n.type === "INSTANCE"
-            ) {
-              n.x = Math.round(n.x);
-              n.y = Math.round(n.y);
-              n.resize(Math.round(n.width), Math.round(n.height));
-              n.paddingLeft = Math.round(n.paddingLeft);
-              n.paddingRight = Math.round(n.paddingRight);
-              n.paddingTop = Math.round(n.paddingTop);
-              n.paddingBottom = Math.round(n.paddingBottom);
-              n.itemSpacing = Math.round(n.itemSpacing);
-              n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
+      figma.root.children.flatMap((pageNode) =>
+        pageNode.selection.forEach(async (node) => {
+          node.x = Math.round(node.x);
+          node.y = Math.round(node.y);
+          node.resize(Math.round(node.width), Math.round(node.height));
+          if (nodetypes.includes(node.type)) {
+            node.x = Math.round(node.x);
+            node.y = Math.round(node.y);
+            node.resize(Math.round(node.width), Math.round(node.height));
+            for (let property of propertiesToRound) {
+              node[property] = Math.round(node[property]);
             }
-            if (
-              n.type === "VECTOR" ||
-              n.type === "STAR" ||
-              n.type === "ELLIPSE" ||
-              n.type === "RECTANGLE" ||
-              n.type === "POLYGON"
-            ) {
-              n.x = Math.round(n.x);
-              n.y = Math.round(n.y);
-              n.resize(Math.round(n.width), Math.round(n.height));
-              n.cornerRadius = Math.round(parseInt(n.cornerRadius.toString()));
-            }
-            if (n.type === "TEXT") {
-              await figma.loadFontAsync(n.fontName as FontName);
-              n.x = Math.round(n.x);
-              n.y = Math.round(n.y);
-              n.resize(Math.round(n.width), Math.round(n.height));
-              n.strokeWeight = Math.round(n.strokeWeight);
-              n.fontSize = Math.round(parseFloat(n.fontSize.toString()));
-              let LH = n.getRangeLineHeight(0, selection.length);
-              if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
-                n.setRangeLineHeight(0, n.characters.length, {
-                  value: Math.round(LH.value),
-                  unit: "PIXELS",
-                });
+            node.cornerRadius = Math.round(Number(node.cornerRadius));
+            for (let innerNode of pageNode.selection.children) {
+              innerNode.x = Math.round(innerNode.x);
+              innerNode.y = Math.round(innerNode.y);
+              innerNode.resize(
+                Math.round(innerNode.width),
+                Math.round(innerNode.height)
+              );
+              if (nodetypes.includes(innerNode.type)) {
+                for (let property of propertiesToRound) {
+                  innerNode[property] = Math.round(innerNode[property]);
+                }
+                innerNode.cornerRadius = Math.round(
+                  Number(innerNode.cornerRadius)
+                );
               }
-              let LS = n.getRangeLetterSpacing(0, selection.length);
-              n.setRangeLetterSpacing(0, n.characters.length, {
-                value: Math.round(LS.value),
-                unit: "PIXELS",
-              });
-              n.paragraphSpacing = Math.round(n.paragraphSpacing);
+              if (nodeshapetypes.includes(innerNode.type)) {
+                innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
+                innerNode.cornerRadius = Math.round(
+                  Number(innerNode.cornerRadius)
+                );
+              }
+              if (innerNode.type === "TEXT") {
+                await figma.loadFontAsync(innerNode.fontName as FontName);
+                innerNode.strokeWeight = Math.round(innerNode.strokeWeight);
+                innerNode.fontSize = Math.round(Number(innerNode.fontSize));
+                let LH = innerNode.getRangeLineHeight(0, selection.length);
+                if (LH.unit !== "AUTO" || LH.unit !== "PERCENT") {
+                  innerNode.setRangeLineHeight(0, innerNode.characters.length, {
+                    value: Math.round(LH.value),
+                    unit: "PIXELS",
+                  });
+                }
+                let LS = innerNode.getRangeLetterSpacing(0, selection.length);
+                innerNode.setRangeLetterSpacing(
+                  0,
+                  innerNode.characters.length,
+                  {
+                    value: Math.round(LS.value),
+                    unit: "PIXELS",
+                  }
+                );
+                innerNode.paragraphSpacing = Math.round(
+                  innerNode.paragraphSpacing
+                );
+              }
             }
           }
-        }
-      }
+        })
+      );
       return Promise.resolve("Done!");
     }
     roundPixels();
